b5743b0ecfad868e771cc6aa451f1fa7906261e8
dlwodud04 <lee72217847@gmail.com>
2023-03-20 20:04:00 +0900

    23/03/20

---
+++ codeup/6055.py
@@ -0,0 +1,5 @@
+a,b = map(int,input().split())
+if a or b :
+    print(True)
+else : 
+    print(False)
---
+++ codeup/6079.py
@@ -0,0 +1,9 @@
+a = int(input())
+b = 0
+c = 0
+while True :
+    c +=b
+    if c >= a:
+        break
+    b +=1
+print(b)
